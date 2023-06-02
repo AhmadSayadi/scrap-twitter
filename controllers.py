bda809ed53da90b41a75af480b85cd5c161479fe
Ahmad Sayadi <ahmad.sayadi@dwidasa.com>
2023-06-02 14:43:14 +0700

    fix scrap twitter

--- controllers.py
+++ controllers.py
@@ -4,77 +4,34 @@ from flask import request
 class Controller:
     def gettwit(self):
         self.store_tweets=[]
-        query = "Ansor Jatim"
-        limit = 100
-        for tweet in twitterScraper.TwitterSearchScraper(query).get_items():
-            if len(self.store_tweets) == limit:
-                break
-            else:
-                result =vartweet(tweet)
-                self.store_tweets.append(result)
-        return {
-            'status_code':00, #Sukses
-            'msg':"success",
-            'data': self.store_tweets
-        }, 200
-    def gettwit3(self):
-        self.store_tweets=[]
-        query = "from:lviikmatin_"
-        limit = 100
-        request_data = request.get_json()
-
-        query = request_data['query']
-
-
         if request.method == 'POST':
+            request_data = request.get_json()
+            query = request_data['query']
+            limit = request_data['limit']
+            jenis = request_data['jenis']
+
+            if(jenis=='Hashtag') :
+                for tweet in twitterScraper.TwitterHashtagScraper(query).get_items():
+                    if len(self.store_tweets) == limit:
+                        break
+                    else:
+                        self.store_tweets.append(tweet)
+            if(jenis=='Cari Nama') :
+                query = "'from:"+request_data['query']+"'"
+                for tweet in twitterScraper.TwitterHashtagScraper(query).get_items():
+                    if len(self.store_tweets) == limit:
+                        break
+                    else:
+                        self.store_tweets.append(tweet)
+            else :
+                for tweet in twitterScraper.TwitterSearchScraper(query).get_items():
+                    if len(self.store_tweets) == limit:
+                        break
+                    else:
+                        self.store_tweets.append(tweet)
             return {
+                'jenis':jenis,
                 'status_code':00, #Sukses
                 'msg':"success",
-                'data':query
-                # 'data': //self.store_tweets
+                'data': self.store_tweets
             }, 200
-
-
-
-        
-
-def vartweet(tweet):
-       data ={
-            "id": tweet.id,
-            "url": str(tweet.url),
-            "date": str(tweet.date),
-            "raw_content": tweet.rawContent,
-            "rendered_content": tweet.renderedContent,
-            "user": str(tweet.user),
-            "replyCount": tweet.replyCount,
-            "retweetCount": tweet.retweetCount,
-            "likeCount": tweet.likeCount,
-            "quoteCount": tweet.quoteCount,
-            "conversationId": tweet.conversationId,
-            "lang": tweet.lang,
-            "source": tweet.source,
-            "sourceUrl": tweet.sourceUrl,
-            "sourceLabel": str(tweet.sourceLabel),
-            "links": str(tweet.links),
-            "media": str(tweet.media),
-            "retweetedTweet": str(tweet.retweetedTweet),
-            "quotedTweet": str(tweet.quotedTweet),
-            "inReplyToTweetId": str(tweet.inReplyToTweetId),
-            "inReplyToUser": str(tweet.inReplyToUser),
-            "mentionedUsers": str(tweet.mentionedUsers),
-            "coordinates": tweet.coordinates,
-            "place": tweet.place,
-            "hashtags": tweet.hashtags,
-            "cashtags": tweet.cashtags,
-            "card": str(tweet.card),
-            "viewCount": tweet.viewCount,
-            "vibe": tweet.vibe,
-            "bookmarkCount": tweet.bookmarkCount,
-            "username": tweet.username,
-            "outlinks": tweet.outlinks,
-            "outlinksss": tweet.outlinksss,
-            "tcooutlinks": tweet.tcooutlinks,
-            "tcooutlinksss": tweet.tcooutlinksss,
-            "content": tweet.content
-        }
-       return data
